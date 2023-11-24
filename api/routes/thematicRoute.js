fee805b23a45a71d749526c935894d6addcd9c99
Miyuki62 <perso.stevenrichart@gmail.com>
2023-11-24 10:06:41 +0100

    changement au niveau des thematicRoute put et delete

--- api/routes/thematicRoute.js
+++ api/routes/thematicRoute.js
@@ -12,21 +12,26 @@ app.get('/get_thematic', async (req, res) => {
     }
 });
 
-app.delete('/delete_thematic', async (req, res) => {
-    const thematic = getThematic(req.params.thematicId);
-
-    if (!thematic) return res.status(404).json({});
-    const index = thematics.indexOf(thematic);
-    thematics.splice(index, 1);
-    res.status(200).json({});
+app.delete('/delete_thematic/:idThematic', async (req, res) => {
+    try {
+        const thematic = await thematicModel.findByIdAndDelete(req.params.idThematic);
+        if (!thematic) return res.status(404).json({});
+        res.status(200).json({});
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 });
 
-app.put('/put_thematic', async (req, res) => {
-    const thematic = getThematic(req.params.thematicId);
-
-    if (!thematic) return res.status(404).json({});
-    thematic.name = req.body.name;
-    res.json(thematic);
+app.put('/put_thematic/:idThematic', async (req, res) => {
+    try {
+        const thematic = await thematicModel.findByIdAndUpdate(req.params.idThematic, { name: req.body.nom }, { new: true });
+        if (!thematic) return res.status(404).json({});
+        res.json(thematic);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 });
 
 module.exports = router;
