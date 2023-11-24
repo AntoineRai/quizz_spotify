dca9f93f4e011f2b671c015bf8fcf1297e157d23
Miyuki62 <perso.stevenrichart@gmail.com>
2023-11-24 09:36:14 +0100

    mise en place du delete et du put

--- api/routes/thematicRoute.js
+++ api/routes/thematicRoute.js
@@ -4,6 +4,23 @@ const Thematic = require('../models/Thematic');
 
 const app = require('./startApp');
 
-router.get('/get_thematic', async (req, res) => {
+app.get('/get_thematic', async (req, res) => {
     
+});
+
+app.delete('/delete_thematic', async (req, res) => {
+    const thematic = getThematic(req.params.thematicId);
+
+    if (!thematic) return res.status(404).json({});
+    const index = thematics.indexOf(thematic);
+    thematics.splice(index, 1);
+    res.status(200).json({});
+});
+
+app.put('/put_thematic', async (req, res) => {
+    const thematic = getThematic(req.params.thematicId);
+
+    if (!thematic) return res.status(404).json({});
+    thematic.name = req.body.name;
+    res.json(thematic);
 });
