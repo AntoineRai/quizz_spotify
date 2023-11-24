93237486293e912dbced0f1bad7c60219d37a469
tzrph <TzRPheonix@gmail.com>
2023-11-24 09:30:21 +0100

    route

--- api/routes/thematicRoute.js
+++ api/routes/thematicRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Thematic = require('../models/Thematic');
 
+const app = require('./startApp');
+
 router.get('/get_thematic', async (req, res) => {
     
 });
---
+++ api/startApp.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
