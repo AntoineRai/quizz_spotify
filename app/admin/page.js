dee2e99b5eb38e69b4609333e5bf087b34c2df97
Antoine <antoine.raimand@gmail.com>
2023-11-24 11:01:01 +0100

    Add update and delete functionality to Thematics component

--- app/admin/page.js
+++ app/admin/page.js
@@ -9,6 +9,7 @@ const Thematics = () => {
     url: "",
     spotifyID: "",
   });
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     const fetchThematics = async () => {
@@ -38,13 +39,55 @@ const Thematics = () => {
     });
   };
 
-  const handleModify = () => {
-    // Ajoutez ici la logique pour la modification du thème avec formData
+  const handleUpdate = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8888/thematic/put_thematic/${selectedTheme._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            idThematic: formData.spotifyID,
+            nom: formData.nom,
+            url: formData.url,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        // Mettez à jour la liste des thématiques après la modification
+        const updatedThemes = themes.map((theme) =>
+          theme._id === selectedTheme._id ? formData : theme
+        );
+        setThemes(updatedThemes);
+
+        setEditing(false);
+      } else {
+        const errorData = await response.json();
+        alert(`Erreur : ${errorData.message}`);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la modification de la thématique :", error);
+    }
     console.log("Modifier :", formData);
   };
 
   const handleDelete = () => {
-    // Ajoutez ici la logique pour la suppression du thème
+    fetch(
+      `http://localhost:8888/thematic/delete_thematic/${selectedTheme._id}`,
+      {
+        method: "DELETE",
+      }
+    ).then((response) => {
+      if (response.ok) {
+        setThemes(themes.filter((theme) => theme._id !== selectedTheme._id));
+        setSelectedTheme(null);
+      } else {
+        console.error("Erreur lors de la suppression :", response);
+      }
+    });
     console.log("Supprimer :", selectedTheme);
   };
 
@@ -55,17 +98,20 @@ const Thematics = () => {
         return;
       }
 
-      const response = await fetch("http://localhost:8888/thematic/add_thematic", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nom: formData.nom,
-          url: formData.url,
-          idThematic: formData.spotifyID,
-        }),
-      });
+      const response = await fetch(
+        "http://localhost:8888/thematic/add_thematic",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nom: formData.nom,
+            url: formData.url,
+            idThematic: formData.spotifyID,
+          }),
+        }
+      );
 
       if (response.ok) {
         const data = await response.json();
@@ -81,6 +127,19 @@ const Thematics = () => {
     } catch (error) {
       console.error("Erreur lors de l'ajout de la thématique :", error);
     }
+  };
+
+  const handleModify = () => {
+    setEditing(true);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    setFormData({
+      nom: "",
+      url: "",
+      spotifyID: "",
+    });
   };
 
   return (
@@ -104,33 +163,77 @@ const Thematics = () => {
           {selectedTheme && (
             <div className="flex flex-col items-center">
               <h2 className="text-center font-bold">Votre thème:</h2>
-              <h2>
-                <u>Le nom:</u> {selectedTheme.nom}
-              </h2>
-              <img
-                src={selectedTheme.url}
-                alt={selectedTheme.nom}
-                className="max-w-400 h-auto"
-              />
-              <p>
-                <u>ID Spotify</u> {selectedTheme.idThematic}
-              </p>
-              <p>
-                <u>ID Mongo:</u> {selectedTheme._id}
-              </p>
+              {editing ? (
+                // Si en mode édition, affiche les champs de texte modifiables
+                <>
+                  <input
+                    type="text"
+                    name="nom"
+                    placeholder="Nom"
+                    value={formData.nom}
+                    onChange={handleChange}
+                    className="m-2 p-2"
+                  />
+                  <input
+                    type="text"
+                    name="url"
+                    placeholder="URL de l'image"
+                    value={formData.url}
+                    onChange={handleChange}
+                    className="m-2 p-2"
+                  />
+                  <input
+                    type="text"
+                    name="spotifyID"
+                    placeholder="ID Spotify"
+                    value={formData.spotifyID}
+                    onChange={handleChange}
+                    className="m-2 p-2"
+                  />
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 m-2 rounded-lg"
+                    onClick={handleUpdate}
+                  >
+                    Mettre à jour
+                  </button>
+                  <button
+                    className="bg-gray-500 text-white px-2 py-1 m-2 rounded-lg"
+                    onClick={handleCancel}
+                  >
+                    Annuler
+                  </button>
+                </>
+              ) : (
+                <>
+                  <h2>
+                    <u>Le nom:</u> {selectedTheme.nom}
+                  </h2>
+                  <img
+                    src={selectedTheme.url}
+                    alt={selectedTheme.nom}
+                    className="max-w-400 h-auto"
+                  />
+                  <p>
+                    <u>ID Spotify</u> {selectedTheme.idThematic}
+                  </p>
+                  <p>
+                    <u>ID Mongo:</u> {selectedTheme._id}
+                  </p>
 
-              <button
-                className="bg-blue-500 text-white px-2 py-1 m-2 rounded-lg"
-                onClick={handleModify}
-              >
-                Modifier
-              </button>
-              <button
-                className="bg-red-500 text-white px-2 py-1 m-2 rounded-lg"
-                onClick={handleDelete}
-              >
-                Supprimer
-              </button>
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 m-2 rounded-lg"
+                    onClick={handleModify}
+                  >
+                    Modifier
+                  </button>
+                  <button
+                    className="bg-red-500 text-white px-2 py-1 m-2 rounded-lg"
+                    onClick={handleDelete}
+                  >
+                    Supprimer
+                  </button>
+                </>
+              )}
             </div>
           )}
         </div>
