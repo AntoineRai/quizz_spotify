6d36b30d76d6057b9367726dc8e7f183b8660e75
Miyuki62 <perso.stevenrichart@gmail.com>
2023-12-15 16:27:06 +0100

    Fix split function to include "(" in currentTitle1

--- app/thematique/[playlistId]/multi/page.js
+++ app/thematique/[playlistId]/multi/page.js
@@ -37,7 +37,7 @@ const Page = ({ params }) => {
       .replace(/\s+/g, ''); //user answer is formatted
       
       const wordsToReplace = listwordban; //words to replace
-      const currentTitle1 = currentTitle.split(" - ")[0];
+      const currentTitle1 = currentTitle.split(" - ","(")[0];
       let formatesongtitle = currentTitle1; 
       wordsToReplace.forEach(word => {
         formatesongtitle = formatesongtitle.replace(word, ""); //remove the unwanted words
